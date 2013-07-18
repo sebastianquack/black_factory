9d7b5c7299c592f0b2faa66afa29302d56a02eca
Sebastian Quack <sebastianquack@gmail.com>
2013-07-18 11:53:18 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlackFactory::Application.config.secret_token = 'ffe82dd0afd5c4229709055c69c74e952c276efc79e858ed3557634e826ab7d755f9af4ff709dfa1432b5f090fea2817e90297f8dd5c2cd0783c5b483495373d'
