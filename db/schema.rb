582777d19eac9118b1cf41fbdbc3792589fef1bb
Sebastian Quack <sebastianquack@gmail.com>
2013-07-18 15:52:01 +0200

    change database name (do not use same database as other project

--- db/schema.rb
+++ db/schema.rb
@@ -13,87 +13,12 @@
 
 ActiveRecord::Schema.define(:version => 20130718123837) do
 
-  create_table "avatars", :force => true do |t|
-    t.string   "name"
-    t.text     "pov_stream_embed"
-    t.datetime "created_at",             :null => false
-    t.datetime "updated_at",             :null => false
-    t.integer  "place_id"
-    t.text     "pov_stream_embed_local"
-  end
-
   create_table "challenges", :force => true do |t|
     t.string   "name"
     t.datetime "time"
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
-  end
-
-  create_table "chat_items", :force => true do |t|
-    t.text     "content"
-    t.string   "ip_address"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-    t.string   "name"
-    t.string   "time_string"
-  end
-
-  create_table "ip_blacklists", :force => true do |t|
-    t.string   "ip_address"
-    t.string   "user_name"
-    t.integer  "status"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  add_index "ip_blacklists", ["ip_address"], :name => "index_ip_blacklists_on_ip_address", :unique => true
-
-  create_table "places", :force => true do |t|
-    t.text     "fix_stream_embed"
-    t.datetime "created_at",             :null => false
-    t.datetime "updated_at",             :null => false
-    t.string   "name"
-    t.text     "fix_stream_embed_local"
-  end
-
-  create_table "settings", :force => true do |t|
-    t.string   "redirect_to"
-    t.integer  "redirect"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-  end
-
-  create_table "suggestions", :force => true do |t|
-    t.text     "content"
-    t.string   "name"
-    t.integer  "score"
-    t.integer  "status"
-    t.string   "ip_address"
-    t.datetime "created_at",        :null => false
-    t.datetime "updated_at",        :null => false
-    t.string   "user_hash"
-    t.integer  "avatar_id"
-    t.string   "time_string"
-    t.string   "voting_started_at"
-    t.string   "name2"
-  end
-
-  create_table "user_scores", :force => true do |t|
-    t.string   "user_name"
-    t.string   "user_hash"
-    t.integer  "score"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-    t.integer  "avatar_id"
-  end
-
-  create_table "user_votes", :force => true do |t|
-    t.integer  "suggestion_id"
-    t.string   "user_hash"
-    t.integer  "vote"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
   end
 
 end
