f296700d2879174866ca499c91a6f234bf1de62f
retani <holgheiss@gmail.com>
2013-08-01 05:06:16 +0200

    remove typeahead dropdown menu

--- app/assets/javascripts/typeahead.js
+++ app/assets/javascripts/typeahead.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
   $('.typeahead-username').typeahead( {
   name: 'usernames', 
   local: [ "Anonym" ],
-  prefetch: '/usernames.json'
+  prefetch: { url: '/usernames.json', ttl: 60 }
   });
 });
