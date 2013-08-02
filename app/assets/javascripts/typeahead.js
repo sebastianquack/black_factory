7d5a4a6b996a6ee5b78aab0a45fe098f3ea504cd
retani <holgheiss@gmail.com>
2013-08-03 01:03:06 +0200

    no changes

--- app/assets/javascripts/typeahead.js
+++ app/assets/javascripts/typeahead.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
   });
   
   
-  $('.typeahead-username').on("typeahead:opened", function () {
+  $('.typeahead-username').on("typeahead:opened", function () { 
     var additionalStyles = " font-family: 'La Belle Aurore', cursive; font-size:25px; color:#ccc; left:-2px; top:-2px;"
     var hint_tag = $(".tt-hint");
     hint_tag.attr("style", hint_tag.attr("style") + additionalStyles);
