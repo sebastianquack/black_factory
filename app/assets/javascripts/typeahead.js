f3f3d7b80fc0327117c02af35843af82acf664e0
retani <holgheiss@gmail.com>
2013-08-03 00:08:16 +0200

    nice styling of signatur. grid style for challenge view. proper calculation of score

--- app/assets/javascripts/typeahead.js
+++ app/assets/javascripts/typeahead.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
   $('.typeahead-username').typeahead( {
-  name: 'usernames', 
-  local: [ "Anonym" ],
-  prefetch: { url: '/usernames.json', ttl: 60 }
+	  name: 'usernames', 
+	  local: [ "Anonym" ],
+	  prefetch: { url: '/usernames.json', ttl: 60 }
   });
+  
+  
+  $('.typeahead-username').on("typeahead:opened", function () {
+    var additionalStyles = " font-family: 'La Belle Aurore', cursive; font-size:25px; color:#ccc; left:-2px; top:-2px;"
+    var hint_tag = $(".tt-hint");
+    hint_tag.attr("style", hint_tag.attr("style") + additionalStyles);
+  });
+  
 });
--- app/models/vote_cookie.rb
+++ app/models/vote_cookie.rb
@@ -2,11 +2,17 @@ class VoteCookie < ActiveRecord::Base
   attr_accessible :cookiehash, :design_id, :ip, :vote
   belongs_to :design
   
-  before_save :calculate_score, :count_votes
-  before_destroy :calculate_score, :count_votes
+  before_save :count_votes, :calculate_score
+  before_destroy :count_votes, :calculate_score
   
   def calculate_score
-  	score = VoteCookie.where(design_id: self.design_id).sum('vote')
+  	score1 = VoteCookie.where("design_id = ? AND vote = 1", self.design_id).count
+  	score2 = VoteCookie.where("design_id = ? AND vote = 2", self.design_id).count
+  	score3 = VoteCookie.where("design_id = ? AND vote = 3", self.design_id).count
+  	score4 = VoteCookie.where("design_id = ? AND vote = 4", self.design_id).count
+  	score5 = VoteCookie.where("design_id = ? AND vote = 5", self.design_id).count
+  	vote_count = VoteCookie.where(design_id: self.design_id).count
+  	score = (1*score1 + 2*score2 + 3*score3 + 4*score4 + 5*score5) / vote_count.to_f
   	if Design.exists?(self.design_id)
 		d = Design.find(self.design_id)
 		d.score = score
@@ -15,7 +21,7 @@ class VoteCookie < ActiveRecord::Base
   end
   
   def count_votes
-  	vote_count = VoteCookie.where(design_id: self.design_id).length
+  	vote_count = VoteCookie.where(design_id: self.design_id).count
   	if Design.exists?(self.design_id)
   		d = Design.find(self.design_id)
   		d.vote_count = vote_count
