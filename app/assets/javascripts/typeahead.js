419abf49428ed828d4652bda27d2bc0adb88be10
retani <holgheiss@gmail.com>
2013-08-15 13:43:22 +0200

    more styling, first decoration

--- app/assets/javascripts/typeahead.js
+++ app/assets/javascripts/typeahead.js
@@ -1,8 +1,26 @@
+var usernames;
+
+grabUsernames = function(parsedResponse) {
+	usernames = parsedResponse;
+	return parsedResponse;
+}
+
 $(document).ready(function() {
   $('.typeahead-username').typeahead( {
 	  name: 'usernames', 
 	  local: [ "Anonym" ],
-	  prefetch: { url: '/usernames.json', ttl: 60 }
+	  prefetch: { url: '/usernames.json', ttl: 60, filter: grabUsernames }
+  });
+  
+  $('.typeahead-username').on("keyup focus", function (a,b) {
+  	if ( $.inArray( $(this).val(), usernames ) ) {
+  		$('.typeahead-username-new').show();
+  		$('.typeahead-username-old').hide();
+  	}
+  	else {
+  		$('.typeahead-username-new').hide();
+  		$('.typeahead-username-old').show();
+  	}
   });
   
   
@@ -13,3 +31,4 @@ $(document).ready(function() {
   });
   
 });
+
