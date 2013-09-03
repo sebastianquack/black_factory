0293d918c6821c5f63cc745d59a0ac23b03470f5
Sebastian Quack <sebastianquack@gmail.com>
2013-09-03 16:40:55 +0200

    use images in pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  def formatize text
+    return "" if text.nil?
+    
+    text = text.gsub(/<image (\S+)>/) {
+      begin
+        image = Image.find("#{$1}")
+        image_tag image.image.url(:thumb)
+      rescue
+        "<strong>Image with id #{$1} not found.</strong>"
+      end      
+    }
+
+    text.html_safe
+  end
+
 end
