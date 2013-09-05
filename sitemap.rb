d9064707e725a6cfc1d337a03793944b6a1bb068
retani <holgheiss@gmail.com>
2013-09-05 20:11:42 +0200

    show meu button (einigermaßen). favicons

---
+++ sitemap.rb
@@ -0,0 +1,8 @@
+require 'rubygems'
+require 'sitemap_generator'
+
+SitemapGenerator::Sitemap.default_host = 'http://example.com'
+SitemapGenerator::Sitemap.create do
+  add '/home', :changefreq => 'daily', :priority => 0.9
+  add '/contact_us', :changefreq => 'weekly'
+end
